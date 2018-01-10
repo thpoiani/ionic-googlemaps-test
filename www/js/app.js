2e9334e4819453eb181076521173b67d9a0ef7f9
Thiago Henrique Poiani <thpoiani@gmail.com>
2018-01-10 16:34:19 -0200

    cordova-plugin-googlemaps

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,39 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.run(function($ionicPlatform, $timeout) {
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+  var map = null;
+
+  $ionicPlatform.ready(function() {
+    map = plugin.google.maps.Map.getMap(document.getElementById('map'), {
+      camera: {
+        target: {
+          lat: -21.797646,
+          lng: -48.174644,
+        },
+        zoom: 16
+      },
+    });
+
+    map.on(plugin.google.maps.event.MAP_READY, function() {
+      cordova.fireDocumentEvent('plugin_touch', {});
+    });
+  });
+
+  $ionicPlatform.on('pause', function() {
+    if (map) {
+      map.setDiv(null);
     }
   });
+
+  $ionicPlatform.on('resume', function() {
+    if (map) {
+      map.setDiv(document.getElementById('map'));
+
+      $timeout(function() {
+        cordova.fireDocumentEvent('plugin_touch', {});
+      }, 400);
+    }
+  });
+
 })
